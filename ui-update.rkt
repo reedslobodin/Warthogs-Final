c52d2fdbc0e17e1762f423f6f35416c1d7beb8f0
Khoinie <32655064+Khoinie@users.noreply.github.com>
2017-11-29 11:37:05 -0800

    Update ui-update.rkt

--- ui-update.rkt
+++ ui-update.rkt
@@ -1,8 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname ui-update) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+
 ;FinalProject : Melody Maker
 
+(require racket/list)
+(define-struct ws [vol songpos playing? fam tempo note-list w9 timer1 timer2])
 
 ;a World State is make-ws
 ;where (make-ws) is (make-ws vol songpos playing? fam tempo note-list w9 timer1 timer2)
@@ -18,11 +21,10 @@
 ; (list (list Boolean list-of-booleans))
 
 
-;timer1 takes in a number from 0 to 1
-;and timer2 takes in a number from 0 to 1
+;timer1 takes in a number from 0 to .49
+;and timer2 takes in a number from .49 to .98
 ;--------------------------------------------------------------------------------------------------
-(require racket/list)
-(define-struct ws [vol songpos playing? fam tempo note-list w9 timer1 timer2])
+
 
 ;8x8. Each "window" will be 4 x 8 (row x column)
 
@@ -52,29 +54,32 @@
 
 ;Exists purely for aesthetics
 (define dividers (rectangle 6 BLANK-HEIGHT "solid" bg-color))
-
-;This will be the border to divide the two "windows"
-;Or the border below the two windows
-(define border (rectangle 4 BLANK-HEIGHT "solid" "black"))
+(define border (rectangle 4 BLANK-HEIGHT "solid" "black")) ;;divides the two windows
 (define bot-border (rectangle BLANK-WIDTH 18 "solid" bg-color))
 
+;--------------------------------------------------------------------------------------------------
+
+;creates x coordinates for image placement
+;Number --> Number
 (define (x-coor x)
   (cond
     [(= x 1) 75]
-    [else (- (* 150 x) 75)]
-    ))
+    [else (- (* 150 x) 75)]))
+(check-expect (x-coor 1) 75)
+(check-expect (x-coor 5) 675)
+
+;creates y coordinates for image placement
+;Number --> Number
 (define (y-coor y)
   (cond
     [(= y 1) 14.5]
     [else (- (* 29 y) 14.5)]))
-
-;A function I created to make an image with 8 rows x 8 columns of dots
-;(if I didn't do this, there would be 64 place-image functions) to just define
-;the background. ... That would be a lot of parenthesis
+(check-expect (y-coor 1) 14.5)
+(check-expect (y-coor 4) 101.5)
 
 ;bg-image-dots takes in a list-of-numbers
 ;that is either '() or (cons Number List-of-numbers)
-;List --> Image
+;List-of-numbers --> Image
 (define (bg-image-dots LIST)
   (cond
     [(empty? LIST) blank]
@@ -86,8 +91,7 @@
           (place-image dot (x-coor 6) (y-coor (+ 1 (first LIST)))
           (place-image dot (x-coor 7) (y-coor (+ 1 (first LIST)))
           (place-image dot (x-coor 8) (y-coor (+ 1 (first LIST)))
-                             (bg-image-dots (rest LIST))
-    ))))))))]))
+                             (bg-image-dots (rest LIST))))))))))]))
 
 (check-expect (bg-image-dots (list 0))
               (place-image dot (x-coor 1) (y-coor 1)
@@ -98,30 +102,31 @@
               (place-image dot (x-coor 6) (y-coor 1)
               (place-image dot (x-coor 7) (y-coor 1)
               (place-image dot (x-coor 8) (y-coor 1)
-              blank
-              )))))))))
+              blank)))))))))
 
 ;Base-image.
-;Since this will be 8x8, there's a total of 64 dots.
 (define bg-image (bg-image-dots (list 0 1 2 3 4 5 6 7)))
 
-;---------------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------------------------
 
 ;ref is a function that takes in a
 ;List, n1, and n2
 ;where n1 is a number that corresponds with the first position in the lists
 ;and n2 is a number that corresponds with the second position in the lists
-;These two numbers together hone into a single boolean in the WorldState List.
-;List Number Number --> Boolean
 
+;Hones into into a single boolean in the WorldState List.
+;List Number Number --> Boolean
 (define (ref ws n1 n2)
     (list-ref (list-ref ws (sub1 n1)) (sub1 n2)))
+(check-expect (ref (list (list #f #t #t #t) (list #t #f #f #f)) 2 1) #t)
+(check-expect (ref (list (list #f #t #t #t) (list #t #f #f #f)) 1 1) #f)
 
 ;Rect-placer is a function that
 ;takes a WorldState, x, and y
 ;where x is a number representing the first position in the lists
 ;and y is a number representing the second position in the lists
-;This functions places rectangles if the certain position in the WorldState is #t
+
+;Places rectangles if a certain position in the WorldState is #t
 ;if it's #f, then the rectangle "disappears"
 ;WorldState Number Number --> Image
 (define (rect-placer ws x y)
@@ -155,7 +160,8 @@
                                                     (rect-placer ws (add1 x) y))]
     [else bg-image]
     ))
-;;The to-draw function which deals with different states of the world
+
+;;Takes in a world-state and returns an image. Used with to-draw
 ;World State --> Image
 (define (image1 ws)
   (place-image bot-border (/ BLANK-WIDTH 2) BLANK-HEIGHT
@@ -172,7 +178,7 @@
   (rect-placer ws 1 1)
   ))))))))))))
 
-;Draws the slider, includes the ability to loop.
+;Draws the slider, includes the ability to loop. Used with on-tick
 ;WorldState --> WorldState
 (define (slider ws)
   (make-ws (ws-vol ws) (ws-songpos ws) (ws-playing? ws) (ws-fam ws) (ws-tempo ws)
@@ -188,37 +194,41 @@
               [else   (+ TEMPO2 (ws-timer2 ws))]
               )))
 
+(check-expect (slider (make-ws 1 2 3 4 5 6 7 .49 .60)) (make-ws 1 2 3 4 5 6 7 (+ TEMPO1 .49) (+ TEMPO2 .60)))
+(check-expect (slider (make-ws 1 2 3 4 5 6 7 .60 2)) (make-ws 1 2 3 4 5 6 7 0 .49))
 ;The tempo of either window 1 or window 2
 "REPLACE LATER WITH TEMPO FUNCTION"
 ;(the numbers are currently placeholders for the purpose of testing)
 (define TEMPO1 1/200)
 (define TEMPO2 1/500)
+"How to sync these tempos with pstreams?"
 
 ;;If you ever find the slider looping out of synch, try messing
 ;;with the 0.49. (I don't know why .49 is the way it is, but
 ;;trial and error led me to it.)
 
-;------------------------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------------------------
 
 ;LC (list-checker) is a function that
-;takes in a list, n1, and n2
-;where n1 is a number that represents the first position in a list
-;and n2 is a number that represents the second position in a list
-;It will take the boolean specified by the two numbers and make it either #t--> #f or #f --> #t
+;takes in a list-of-booleans, n1, and n2
+;where n1 is a number that represents the first position in the list
+;and n2 is a number that represents the second position in the list
+
+;Takes the boolean specified by the two numbers and make it either #t--> #f or #f --> #t
 ;List Number Number --> List
-
-;;;;It will be used within mouse events so we could change the states whenever a mouse-event happens
-
-;n1 and n2 are subtracted by one because list position start with 0, but it's easier for me to think it starts at 1.
 (define (LC ws n1 n2)
-  (list-set (ws-w9 ws) (sub1 n1) (list-set (list-ref (ws-w9 ws) (sub1 n1)) (sub1 n2) (not (list-ref (list-ref (ws-w9 ws) (sub1 n1))(sub1 n2))))))
+  (list-set ws (sub1 n1) (list-set (list-ref ws (sub1 n1)) (sub1 n2) (not (list-ref (list-ref ws (sub1 n1))(sub1 n2))))))
+  
+;;;;It will be used within mouse events so we could change the states whenever a mouse-event happens
+;n1 and n2 are subtracted by one because list position start with 0, but it's easier for me to think it starts at 1.
 
+(check-expect (LC (list (list #f #t #f #t) (list #t #t #t #t)) 1 2) (list (list #f #f #f #t) (list #t #t #t #t)))
+(check-expect (LC (list (list #f #t #f #t) (list #t #t #t #t)) 2 4) (list (list #f #t #f #t) (list #t #t #t #f)))
 
 ;x-LC is a function that
-;takes in a number and
-;returns a number for LC to use
+;takes in a number
 ;It will be used within the on-mouse functions,
-;so the input number is the x-coordinate of the mouse
+;so the number is the x-coordinate of the mouse
 ;Number (x-coor) --> Number
 (define (x-LC x)
   (cond
@@ -243,7 +253,7 @@
 (check-expect (y-LC 3) 1)
 (check-expect (y-LC 70) 3)
 (check-expect (y-LC 500) 9)
-;------------------------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------------------------
 
 ;The function mouse-test will handle mouse events
 ;It will change dots/rectangles to rectangles/dots
@@ -254,26 +264,27 @@
     (cond
       [(mouse=? "button-down" key)
        (make-ws (ws-vol ws) (ws-songpos ws) (ws-playing? ws) (ws-fam ws) (ws-tempo ws)
-                (ws-note-list ws) (LC ws (x-LC x) (y-LC y)) (ws-timer1 ws) (ws-timer2 ws))]
+                (ws-note-list ws) (LC (ws-w9 ws) (x-LC x) (y-LC y)) (ws-timer1 ws) (ws-timer2 ws))]
       [else ws]
       ))
-  
 
-;------------------------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------------------------
 (define (test ws)
   (big-bang ws
     [on-mouse mouse-test]
     [on-tick slider 1/100]
     [to-draw image1]))
-;------------------------------------------------------------------------------------------------
+;--------------------------------------------------------------------------------------------------
 (define w9-Initial (list (list #t #f #f #f #f #f #f #f #f) (list #f #t #f #f #f #f #f #f #f) (list #f #f #t #f #f #f #f #f #f) (list #f #f #f #t #f #f #f #f #f)
                          (list #f #f #f #f #t #f #f #f #f) (list #f #f #f #f #f #t #f #f #f) (list #f #f #f #f #f #f #t #f #f) (list #f #f #f #f #f #f #f #t #f)
                          (list #f #f #f #f #f #f #f #f #f)))
 
 (define INITIAL-STATE
-  (make-ws 1 2 3 4 5 6 w9-Initial 0 .47))
+  (make-ws 1 2 3 4 5 6 w9-Initial 0 .49))
 
 (test INITIAL-STATE)
+
+
 
 
 
